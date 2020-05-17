e71a96bdf0c3db7fc774c97196008a3fc76a8ecb
chilldude <tonychenkt@gmail.com>
2020-05-17 10:25:08 -0700

    add diffuse material

--- main.cpp
+++ main.cpp
@@ -7,10 +7,25 @@
 #include <cstdlib>
 #include <ctime>
 
+// Returns a float between 0 and 1
+float get_rand() {
+  return 0.5 * (((double) rand() / (RAND_MAX)) + 1);
+}
+
+vec3 random_point_in_unit_sphere() {
+  vec3 p;
+  do {
+    p = 2.0*vec3(get_rand(), get_rand(), get_rand()) - vec3(1,1,1);
+  } while (p.squared_length() >= 1.0);
+  return p;
+}
+
 vec3 color(const ray& r, hitable *world) {
   hit_record rec;
   if (world->hit(r, 0.0, MAXFLOAT, rec)) {
-    return 0.5 * vec3(rec.normal.x()+1.0, rec.normal.y()+1.0, rec.normal.z()+1.0);
+    vec3 target = rec.p + rec.normal + random_point_in_unit_sphere();
+    //return 0.5 * vec3(rec.normal.x()+1.0, rec.normal.y()+1.0, rec.normal.z()+1.0);
+    return  0.5 * color(ray(rec.p, target-rec.p), world);
   } else {
     //vec3 unit_direction = r.direction().make_unit_vector();
     vec3 unit_direction = unit_vector(r.direction());
@@ -28,7 +43,7 @@ int main() {
   srand(time(NULL));
   int nx = 800;
   int ny = 400;
-  int ns = 100;
+  int ns = 15;
 
   // Define size and max color for image
   std::cout << "P3\n" << nx << " " << ny << "\n255\n";
@@ -48,8 +63,8 @@ int main() {
 
       // add simple anti-aliasing
       for (int s=0; s < ns; s++) {
-        float u = float(i + (((double) rand() / (RAND_MAX)) + 1)) / float(nx);
-        float v = float(j + (((double) rand() / (RAND_MAX)) + 1)) / float(ny);
+        float u = float(i + get_rand()) / float(nx);
+        float v = float(j + get_rand()) / float(ny);
 
         ray r = cam.get_ray(u, v);
         col += color(r, world);
@@ -60,6 +75,7 @@ int main() {
       int ig = int(255.99*col[1]);
       int ib = int(255.99*col[2]);
       std::cout << ir << " " << ig << " " << ib << "\n";
+      //std::cout << j << "," << i << "\n";
     }
   }
 }
